38d05336a04e161a75324c8684a881dc256fe43f
Dramatic Amphibion <91826271+Little-coder9@users.noreply.github.com>
2024-08-10 07:16:32 -0400

    Update MovieController.java

--- rest-api-mongodb/src/main/java/com/my_video_site/rest_api_mongodb/Controllers/MovieController.java
+++ rest-api-mongodb/src/main/java/com/my_video_site/rest_api_mongodb/Controllers/MovieController.java
@@ -14,7 +14,7 @@ import com.my_video_site.rest_api_mongodb.Services.MovieService;
 
 
 //@CrossOrigin(origins = "http://localhost:3000")
-@CrossOrigin(origins = {"http://localhost:3000", "https://video-streaming-site-3ml5yjfu3-netra-bahadur-ranas-projects.vercel.app/"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://video-streaming-site-chi.vercel.app"})
 @RestController
 public class MovieController {
 
